60aea0b187f7216e922539be5f2bb4786f9f673f
tanadolMo <tanadol.mo@kkumail.com>
2025-10-14 20:46:12 +0700

    feat: init service project

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import { Elysia } from "elysia";
+import staticPlugin from "@elysiajs/static";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+    .use(await staticPlugin({
+        prefix: '/',
+        assets: 'public',
+        alwaysStatic: true,
+    }))
+    .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
